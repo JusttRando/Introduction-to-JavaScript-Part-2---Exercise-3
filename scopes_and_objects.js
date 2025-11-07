5f246ef1011a541554fb4bc260b714dff0cc85b5
Rando <justtrando@gmail.com>
2025-11-07 12:14:48 +0200

    Obect and scope

---
+++ scopes_and_objects.js
@@ -0,0 +1,21 @@
+// TODO: Create a global variable called globalCount
+
+let globalCount = 4
+
+// TODO: Create a function that demonstrates local scope
+
+function localCount(num)
+{
+    num + 5
+}
+
+// TODO: Create a function that tries to modify both variables
+
+function stateCount(local,global)
+{
+    return local = 6 , global = 9 
+}
+
+// TODO: Create a Student constructor function
+// TODO: Create several student instances
+// TODO: Create an object literal with nested properties
